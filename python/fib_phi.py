6b78e05ce669831a6286a1d6f2d7bd54c33e17a8
Jesse Farmer <jesse@20bits.com>
2025-12-03 10:46:15 -0800

    Python: unicode variable names for laughs

--- python/fib_phi.py
+++ python/fib_phi.py
@@ -1,4 +1,12 @@
 from phi_rational import *
 
+# NOTE: Since ψ = -1/φ, we have ψ^n = (-1)^n / φ^n. This means we could
+# compute φ^n once and derive ψ^n via reciprocal, cutting exponentiations
+# in half. This makes it almost as fast as fib_matrix with numpy!
+
 def fib_phi(n):
-    return int((PhiRational(0,1)**n - PhiRational(1,-1)**n) / PhiRational(-1, 2))
+    φ = PhiRational(0, 1)
+    ψ = PhiRational(1, -1)
+    sqrt5 = PhiRational(-1, 2)
+
+    return int((φ**n - ψ**n) / sqrt5)
